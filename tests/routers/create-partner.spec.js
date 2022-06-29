c718590088076f0849639761660c003cd8238dff
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-06-29 00:35:14 -0300

    test refact

--- tests/routers/create-partner.spec.js
+++ tests/routers/create-partner.spec.js
@@ -1,6 +1,12 @@
 class CreateRouter {
   Route (httpRequest) {
-    if (!httpRequest.body.id) {
+    const { id, tradingName, ownerName, document, coverageArea, address } = httpRequest.body
+    if (!id ||
+      !tradingName ||
+      !ownerName ||
+      !document ||
+      !coverageArea ||
+      !address) {
       return {
         statusCode: 400
       }
@@ -9,12 +15,12 @@ class CreateRouter {
 }
 
 describe('create partner', () => {
-  test('Should return 400 if no id is provided', () => {
+  test('Should return 400 if no trading name is provided', () => {
     const sut = new CreateRouter()
     const httpRequest = {
       body: {
-        id: false,
-        tradingName: 'Adega da Cerveja - Pinheiros',
+        id: 1,
+        tradingName: false,
         ownerName: 'Zé da Silva',
         document: '1432132123891/0001',
         coverageArea: {
@@ -28,6 +34,97 @@ describe('create partner', () => {
           type: 'Point',
           coordinates: [-46.57421, -21.785741]
         }
+      }
+    }
+    const httpResponse = sut.Route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no owner name is provided', () => {
+    const sut = new CreateRouter()
+    const httpRequest = {
+      body: {
+        id: 1,
+        tradingName: 'Adega da Cerveja - Pinheiros',
+        ownerName: false,
+        document: '1432132123891/0001',
+        coverageArea: {
+          type: 'MultiPolygon',
+          coordinates: [
+            [[[30, 20], [45, 40], [10, 40], [30, 20]]],
+            [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]
+          ]
+        },
+        address: {
+          type: 'Point',
+          coordinates: [-46.57421, -21.785741]
+        }
+      }
+    }
+    const httpResponse = sut.Route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no document is provided', () => {
+    const sut = new CreateRouter()
+    const httpRequest = {
+      body: {
+        id: 1,
+        tradingName: 'Adega da Cerveja - Pinheiros',
+        ownerName: 'Zé da Silva',
+        document: false,
+        coverageArea: {
+          type: 'MultiPolygon',
+          coordinates: [
+            [[[30, 20], [45, 40], [10, 40], [30, 20]]],
+            [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]
+          ]
+        },
+        address: {
+          type: 'Point',
+          coordinates: [-46.57421, -21.785741]
+        }
+      }
+    }
+    const httpResponse = sut.Route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no coverage area is provided', () => {
+    const sut = new CreateRouter()
+    const httpRequest = {
+      body: {
+        id: 1,
+        tradingName: 'Adega da Cerveja - Pinheiros',
+        ownerName: 'Zé da Silva',
+        document: '1432132123891/0001',
+        coverageArea: false,
+        address: {
+          type: 'Point',
+          coordinates: [-46.57421, -21.785741]
+        }
+      }
+    }
+    const httpResponse = sut.Route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no coverage area is provided', () => {
+    const sut = new CreateRouter()
+    const httpRequest = {
+      body: {
+        id: 1,
+        tradingName: 'Adega da Cerveja - Pinheiros',
+        ownerName: 'Zé da Silva',
+        document: '1432132123891/0001',
+        coverageArea: {
+          type: 'MultiPolygon',
+          coordinates: [
+            [[[30, 20], [45, 40], [10, 40], [30, 20]]],
+            [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]
+          ]
+        },
+        address: false
       }
     }
     const httpResponse = sut.Route(httpRequest)
