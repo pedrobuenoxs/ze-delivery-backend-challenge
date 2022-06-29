5ae7802044bd9818f21389fd8f0df92f85c0a569
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-06-29 00:12:38 -0300

    add create-partner test

--- src/api/routes/load-partner.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/getOne/:id', (req, res) => {
-  res.send('Get by ID API')
-})
-
-module.exports = router
--- src/api/routes/search-partner.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/get/:lat&long', (req, res) => {
-  res.send('Get by ID API')
-})
-
-module.exports = router
