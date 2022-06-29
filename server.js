c7fc6153fe7efb215343f851b7b862bb6921a2eb
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-06-28 23:32:33 -0300

    server file moved to root

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require('./src/api/routes/create-partner.route')
+
+// RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
+app.use(express.json())
+
+app.use('/api', routes)
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})
