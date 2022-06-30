b075f553b9cd84bce3fcece8b8023826a24f7bc3
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-06-30 00:45:18 -0300

    still not done

--- src/api/routers/create-partner.route.js
+++ src/api/routers/create-partner.route.js
@@ -1,13 +1,15 @@
 const HttpResponse = require('../helpers/http-responses')
 module.exports = class CreateRouter {
-  Route (httpRequest) {
+  async Route (httpRequest) {
     if (!httpRequest || !httpRequest.body) return HttpResponse.serverError()
     const { id, tradingName, ownerName, document, coverageArea, address } = httpRequest.body
     if (!id) return HttpResponse.badRequest('id')
     if (!tradingName) return HttpResponse.badRequest('tradingName')
     if (!ownerName) return HttpResponse.badRequest('ownerName')
     if (!document) return HttpResponse.badRequest('document')
-    if (!coverageArea) return HttpResponse.badRequest('coverageArea')
-    if (!address) return HttpResponse.badRequest('address')
+    if (!coverageArea || coverageArea.type !== 'MultiPolygon') return HttpResponse.badRequest('coverageArea', httpRequest.body)
+    if (!address || address.type !== 'Point') return HttpResponse.badRequest('address', httpRequest.body)
+
+    return HttpResponse.created(httpRequest)
   }
 }
