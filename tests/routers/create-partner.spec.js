a2b249dd6d30ae18c3a687e2c669dfde12b56d3e
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-06-30 00:46:43 -0300

    add 11 unit tests to create a partner

--- tests/routers/create-partner.spec.js
+++ tests/routers/create-partner.spec.js
@@ -1,12 +1,15 @@
 const CreateRouter = require('../../src/api/routers/create-partner.route')
 const MissingParamsError = require('../../src/api/helpers/missingParams-error')
 
+const makeSut = () => {
+  return new CreateRouter()
+}
+
 describe('create partner', () => {
-  test('Should return 400 if no id is provided', () => {
-    const sut = new CreateRouter()
+  test('Should return 400 if no id is provided', async () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
-        id: false,
         tradingName: 'Adega da Cerveja - Pinheiros',
         ownerName: 'Zé da Silva',
         document: '1432132123891/0001',
@@ -23,17 +26,16 @@ describe('create partner', () => {
         }
       }
     }
-    const httpResponse = sut.Route(httpRequest)
+    const httpResponse = await sut.Route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamsError('id'))
+    expect(httpResponse.err).toEqual(new MissingParamsError('id'))
   })
 
-  test('Should return 400 if no trading name is provided', () => {
-    const sut = new CreateRouter()
+  test('Should return 400 if no trading name is provided', async () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
         id: 1,
-        tradingName: false,
         ownerName: 'Zé da Silva',
         document: '1432132123891/0001',
         coverageArea: {
@@ -49,18 +51,17 @@ describe('create partner', () => {
         }
       }
     }
-    const httpResponse = sut.Route(httpRequest)
+    const httpResponse = await sut.Route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamsError('tradingName'))
+    expect(httpResponse.err).toEqual(new MissingParamsError('tradingName'))
   })
 
-  test('Should return 400 if no owner name is provided', () => {
-    const sut = new CreateRouter()
+  test('Should return 400 if no owner name is provided', async () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
         id: 1,
         tradingName: 'Adega da Cerveja - Pinheiros',
-        ownerName: false,
         document: '1432132123891/0001',
         coverageArea: {
           type: 'MultiPolygon',
@@ -75,19 +76,18 @@ describe('create partner', () => {
         }
       }
     }
-    const httpResponse = sut.Route(httpRequest)
+    const httpResponse = await sut.Route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamsError('ownerName'))
+    expect(httpResponse.err).toEqual(new MissingParamsError('ownerName'))
   })
 
-  test('Should return 400 if no document is provided', () => {
-    const sut = new CreateRouter()
+  test('Should return 400 if no document is provided', async () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
         id: 1,
         tradingName: 'Adega da Cerveja - Pinheiros',
         ownerName: 'Zé da Silva',
-        document: false,
         coverageArea: {
           type: 'MultiPolygon',
           coordinates: [
@@ -101,33 +101,57 @@ describe('create partner', () => {
         }
       }
     }
-    const httpResponse = sut.Route(httpRequest)
+    const httpResponse = await sut.Route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamsError('document'))
+    expect(httpResponse.err).toEqual(new MissingParamsError('document'))
   })
 
-  test('Should return 400 if no coverage area is provided', () => {
-    const sut = new CreateRouter()
+  test('Should return 400 if no coverage area is provided', async () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
         id: 1,
         tradingName: 'Adega da Cerveja - Pinheiros',
         ownerName: 'Zé da Silva',
         document: '1432132123891/0001',
-        coverageArea: false,
         address: {
           type: 'Point',
           coordinates: [-46.57421, -21.785741]
         }
       }
     }
-    const httpResponse = sut.Route(httpRequest)
+    const httpResponse = await sut.Route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamsError('coverageArea'))
+    expect(httpResponse.err).toEqual(new MissingParamsError('coverageArea'))
   })
 
-  test('Should return 400 if no address area is provided', () => {
-    const sut = new CreateRouter()
+  test('Should return 400 if coverage area does not follow GeoJSON MultiPolygon ', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        id: 1,
+        tradingName: 'Adega da Cerveja - Pinheiros',
+        ownerName: 'Zé da Silva',
+        document: '1432132123891/0001',
+        coverageArea: {
+          type: 'not_MultiPolygon',
+          coordinates: [
+            [[[30, 20], [45, 40], [10, 40], [30, 20]]],
+            [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]
+          ]
+        },
+        address: {
+          type: 'Point',
+          coordinates: [-46.57421, -21.785741]
+        }
+      }
+    }
+    const httpResponse = await sut.Route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body.coverageArea.type).toEqual(httpRequest.body.coverageArea.type)
+  })
+  test('Should return 400 if address area does not follow GeoJSON Point ', async () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
         id: 1,
@@ -141,24 +165,73 @@ describe('create partner', () => {
             [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]
           ]
         },
-        address: false
+        address: {
+          type: 'not_Point',
+          coordinates: [-46.57421, -21.785741]
+        }
       }
     }
-    const httpResponse = sut.Route(httpRequest)
+    const httpResponse = await sut.Route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamsError('address'))
+    expect(httpResponse.body.address.type).toEqual(httpRequest.body.address.type)
   })
 
-  test('Should return 500 if no http request is provided', () => {
-    const sut = new CreateRouter()
-    const httpResponse = sut.Route()
+  test('Should return 400 if no address area is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        id: 1,
+        tradingName: 'Adega da Cerveja - Pinheiros',
+        ownerName: 'Zé da Silva',
+        document: '1432132123891/0001',
+        coverageArea: {
+          type: 'MultiPolygon',
+          coordinates: [
+            [[[30, 20], [45, 40], [10, 40], [30, 20]]],
+            [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]
+          ]
+        }
+      }
+    }
+    const httpResponse = await sut.Route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.err).toEqual(new MissingParamsError('address'))
+  })
+
+  test('Should return 500 if no http request is provided', async () => {
+    const sut = makeSut()
+    const httpResponse = await sut.Route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
-  test('Should return 500 if http has no body', () => {
+  test('Should return 500 if http has no body', async () => {
     const sut = new CreateRouter()
     const httpRequest = { }
-    const httpResponse = sut.Route(httpRequest)
+    const httpResponse = await sut.Route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
+  })
+  test('Should return 201 if request has no problems', async () => {
+    const sut = new CreateRouter()
+    const httpRequest = {
+      body: {
+        id: 1,
+        tradingName: 'Adega da Cerveja - Pinheiros',
+        ownerName: 'Zé da Silva',
+        document: '1432132123891/0001',
+        coverageArea: {
+          type: 'MultiPolygon',
+          coordinates: [
+            [[[30, 20], [45, 40], [10, 40], [30, 20]]],
+            [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]
+          ]
+        },
+        address: {
+          type: 'Point',
+          coordinates: [-46.57421, -21.785741]
+        }
+      }
+    }
+    const httpResponse = await sut.Route(httpRequest)
+    expect(httpResponse.statusCode).toBe(201)
   })
 })
