736a887db45b0690a2b9bf25b7b6e8c5a623820d
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-06 01:19:15 -0300

    add search middleware

--- src/api/routes/routes.js
+++ src/api/routes/routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const Routes = Router()
 const createPartner = require('../middleware/createPartner-middleware')
 const loadPartner = require('../middleware/loadPartner-middleware')
+const searchPartner = require('../middleware/searchPartner-middleware')
 
 Routes.get('/', (request, response) => {
   response.send('hello world')
@@ -11,9 +12,6 @@ Routes.post('/partner', createPartner)
 
 Routes.get('/partner/:id', loadPartner)
 
-Routes.get('/partner/:long:lat', (request, response) => {
-//   const { long, lat } = request.params
-  response.send('hello world')
-})
+Routes.get('/partner/:long/:lat', searchPartner)
 
 module.exports = Routes
