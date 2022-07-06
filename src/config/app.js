8dc58fe06770ff995bf8eb0c5e126347595777e3
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-05 23:13:15 -0300

    now just has express config, do not start the server

--- src/config/app.js
+++ src/config/app.js
@@ -2,12 +2,11 @@ const express = require('express')
 const router = require('../api/routes/routes')
 
 const app = express()
+
 app.use(express.json())
 
-const port = 3000
+// TODO  = adicionar process.env aqui
 
 app.use('/api', router)
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`)
-})
+module.exports = app
