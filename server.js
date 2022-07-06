f1f36bdba6772f80fd642d3d09d74984fce18b1c
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-06 00:07:30 -0300

    remove todo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // TODO =  preciso trazer o server.listen() pra cá
 
 const app = require('./src/config/app')
-const port = 3000
+const port = 3001
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 })
