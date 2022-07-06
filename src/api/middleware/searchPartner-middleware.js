79b679e1ab2503fd30cd4ed7b6ee774469c4efbc
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-06 01:18:50 -0300

    add search middleware

---
+++ src/api/middleware/searchPartner-middleware.js
@@ -0,0 +1,6 @@
+
+const PartnerRepository = require('../repositories/partner-repository')
+
+module.exports = async (request, response) => {
+  response.json(await new PartnerRepository().Search(request))
+}
