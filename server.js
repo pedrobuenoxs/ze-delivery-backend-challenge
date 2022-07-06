2fe3dbee41aacfd283c8304312283490c5f8c4e8
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-05 23:10:01 -0300

    add server start in a file in root

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-// preciso trazer o server.listen() pra cá
+// TODO =  preciso trazer o server.listen() pra cá
+
+const app = require('./src/config/app')
+const port = 3000
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})
