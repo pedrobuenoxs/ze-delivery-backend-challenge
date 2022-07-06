84b7d3ff6132b2ef77e9a9c0811dbce6846671b8
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-05 23:19:41 -0300

    add try catch

--- src/api/presentation/create-partner.route.js
+++ src/api/presentation/create-partner.route.js
@@ -1,14 +1,18 @@
 const HttpResponse = require('../helpers/http-responses')
 module.exports = class CreateRouter {
   async Route (httpRequest) {
-    if (!httpRequest || !httpRequest.body) return HttpResponse.serverError()
-    const { id, tradingName, ownerName, document, coverageArea, address } = httpRequest.body
-    if (!id) return HttpResponse.badRequest('id')
-    if (!tradingName) return HttpResponse.badRequest('tradingName')
-    if (!ownerName) return HttpResponse.badRequest('ownerName')
-    if (!document) return HttpResponse.badRequest('document')
-    if (!coverageArea || coverageArea.type !== 'MultiPolygon') return HttpResponse.badRequest('coverageArea', httpRequest.body)
-    if (!address || address.type !== 'Point') return HttpResponse.badRequest('address', httpRequest.body)
+    // if (!httpRequest || !httpRequest.body) return HttpResponse.serverError()
+    try {
+      const { id, tradingName, ownerName, document, coverageArea, address } = httpRequest.body
+      if (!id) return HttpResponse.badRequest('id')
+      if (!tradingName) return HttpResponse.badRequest('tradingName')
+      if (!ownerName) return HttpResponse.badRequest('ownerName')
+      if (!document) return HttpResponse.badRequest('document')
+      if (!coverageArea || coverageArea.type !== 'MultiPolygon') return HttpResponse.badRequest('coverageArea', httpRequest.body)
+      if (!address || address.type !== 'Point') return HttpResponse.badRequest('address', httpRequest.body)
+    } catch (error) {
+      return HttpResponse.serverError()
+    }
 
     /*
       TODO = ADICIONAR REPOSITÓRIO
