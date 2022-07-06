fa388b878759a976880565ec99a328f84fd3b8fc
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-07-06 19:23:24 -0300

    test now are correct, i hope

--- src/api/controllers/search-partner-controller.spec.js
+++ src/api/controllers/search-partner-controller.spec.js
@@ -9,26 +9,15 @@ describe('create partner', () => {
   test('Should return 400 if no address is provided', async () => {
     const sut = makeSut()
     const httpRequest = {
-      body: {
-        tradingName: 'Adega da Cerveja - Pinheiros',
-        ownerName: 'Zé da Silva',
-        document: '1432132123891/0001',
-        coverageArea: {
-          type: 'MultiPolygon',
-          coordinates: [
-            [[[30, 20], [45, 40], [10, 40], [30, 20]]],
-            [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]
-          ]
-        }
+      params: {
+
       }
     }
     const httpResponse = await sut.Route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(expect.not.objectContaining({
-      address: {
-        type: expect.anything(),
-        coordinates: expect.anything()
-      }
+      long: expect.anything(),
+      lat: expect.anything()
     }))
     expect(httpResponse.err).toEqual(new MissingParamsError('address'))
   })
@@ -37,17 +26,15 @@ describe('create partner', () => {
     const sut = makeSut()
     const httpRequest = {
       params: {
-        long: 'any',
-        lat: 'any'
+        long: '1',
+        lat: '2'
       }
     }
     const httpResponse = await sut.Route(httpRequest)
     expect(httpResponse.statusCode).toBe(200)
     expect(httpResponse.body).toEqual(expect.objectContaining({
-      params: {
-        long: 'any',
-        lat: 'any'
-      }
+      long: expect.anything(),
+      lat: expect.anything()
     }))
   })
 })
